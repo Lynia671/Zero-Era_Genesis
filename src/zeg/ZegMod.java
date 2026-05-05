7e182387f5107e9c297859b3e04b967833bc995f
Lynia671 <3888172626@qq.com>
2026-05-05 10:35:20 +0800

    Convert to Java mod - Add main ZegMod class

---
+++ src/zeg/ZegMod.java
@@ -0,0 +1,15 @@
+package zeg;
+
+import mindustry.mod.Mod;
+
+public class ZegMod extends Mod {
+    
+    public ZegMod() {
+        super();
+    }
+    
+    @Override
+    public void loadContent() {
+        // Load your content here (blocks, items, units, etc.)
+    }
+}
