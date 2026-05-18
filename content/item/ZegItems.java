0dfb541f4f321e2990f1fb88f00bb28e26ea3be9
PIX-12015 <3985701722@qq.com>
2026-05-18 10:28:19 +0800

    Rename class and update package structure

--- content/item/ZegItems.java
+++ content/item/ZegItems.java
@@ -1,11 +1,11 @@
-package ThisOneMod.content;
+package zeg.content;
 
 import arc.graphics.Color;
 import mindustry.type.Item;
 
-public class ThisItems {
+public class ZegItems {
     public static Item
-            aluminum, bauxite;
+            aluminum, bauxite, crystallineLithium;
 
     public static void load(){
         aluminum = new Item("aluminum", Color.valueOf("dfdfdf")){{  
@@ -16,6 +16,11 @@ public class ThisItems {
         bauxite = new Item("bauxite", Color.valueOf("3b393a")){{  
             hardness = 0;      // int 类型，0.1 取整为 0  
             cost = 1.2f;  
+        }};
+
+        crystallineLithium = new Item("crystalline-lithium", Color.valueOf("dfdfdf")){{  
+            hardness = 3;  
+            cost = 0.95f;  
         }};
         
     }
