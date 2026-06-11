42ae7baa64417375629bc322cf8d17d6b6dc2862
PIX-12015 <3985701722@qq.com>
2026-06-11 15:20:02 +0800

    Add files via upload

---
+++ src/ZegMod/ZegMod.java
@@ -0,0 +1,21 @@
+package ZegMod;
+
+import ZegMod.content.*;
+import mindustry.mod.Mod;
+
+public class ZegMod extends Mod {
+    
+    public ZegMod() {
+        super();
+    }
+    
+    @Override
+    public void loadContent() {
+        // Load your content here (blocks, items, units, etc.)
+        ZegItems.load();
+        ZegUnitType.load();
+        ZegBlocks.load();
+        ZegPlanets.load();
+        ZegTechTree.load();
+    }
+}
